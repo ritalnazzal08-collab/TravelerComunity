08c6f54a39a8528baaee2da8ea90ddc6b1035e41
ritalnazzal <nazzalrital08@gmail.com>
2026-05-11 09:07:50 +0300

    Change in singUp

--- app/src/main/java/com/example/travelercommunity/SignUpActivity.java
+++ app/src/main/java/com/example/travelercommunity/SignUpActivity.java
@@ -1,6 +1,8 @@
 package com.example.travelercommunity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,15 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class SignUpActivity extends AppCompatActivity {
+    private EditText firstName;
+
+    private EditText lastName;
+
+    private EditText emailEt;
+
+    private EditText passwordEt;
+
+    private Button singUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,13 @@ public class SignUpActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        firstName = findViewById(R.id.first_name);
+
+        lastName = findViewById(R.id.last_name);
+
+        emailEt = findViewById(R.id.email_et);
+
+        passwordEt = findViewById(R.id.password_et);
     }
 }
