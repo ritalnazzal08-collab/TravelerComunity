64e94ad327e8d8f0621eac6861bd6e9d1d9ba66b
Mohammad <m.abakhr@gmail.com>
2026-06-07 19:25:03 +0300

    aaaaa

--- app/src/main/java/com/example/travelercommunity/DBRef.java
+++ app/src/main/java/com/example/travelercommunity/DBRef.java
@@ -1,7 +1,5 @@
 package com.example.travelercommunity;
 
-import android.provider.ContactsContract;
-
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
